52af94a3f23f50bd572864248fd7565c9b460b53
Neto test <kimirina0676@mail.ru>
2023-05-08 20:51:51 +0300

    new branch 2

--- src/main/java/ru/netology/CashbackHacker.java
+++ src/main/java/ru/netology/CashbackHacker.java
@@ -1,12 +1,12 @@
 package ru.netology;
 
-public class CashbackHacker {
 
-    public class CashbackHack {
+
+    public class CashbackHacker {
         private final int boundary = 1000;
 
         public int remain(int amount) {
             return boundary - amount % boundary;
         }
     }
-}
+
--- src/test/java/CashbackHackerTest.java
+++ src/test/java/CashbackHackerTest.java
@@ -1,32 +1,32 @@
 import ru.netology.CashbackHacker;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
-public class CashbackHackerTest {
+
     public class CashbackHackerTest {
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void testRemain() {
             CashbackHacker service = new CashbackHacker();
             int amount = 1000;
             int actual = service.remain(amount);
             int expected = 0;
-            assertEquals(actual, expected);
+            assertEquals(expected, actual);
         }
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void testRemainWithCashback() {
             CashbackHacker service = new CashbackHacker();
             int amount = 900;
             int actual = service.remain(amount);
             int expected = 100;
-            assertEquals(actual, expected);
+            assertEquals(expected, actual);
         }
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void testRemain1() {
             CashbackHacker service = new CashbackHacker();
             int amount = 0;
             int actual = service.remain(amount);
             int expected =1000;
-            assertEquals(actual, expected);
+            assertEquals(expected, actual);
         }
 
 
